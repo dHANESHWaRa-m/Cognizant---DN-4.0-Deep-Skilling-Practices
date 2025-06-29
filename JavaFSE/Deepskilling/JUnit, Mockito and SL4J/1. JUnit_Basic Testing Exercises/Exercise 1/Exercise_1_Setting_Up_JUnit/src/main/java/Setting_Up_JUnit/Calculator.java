7393daabe19f859e0d912858f613f2ed2401ea59
DHANESHWARA M <145712965+dHANESHWaRa-m@users.noreply.github.com>
2025-06-30 01:48:15 +0530

    Add files via upload

---
+++ JavaFSE/Deepskilling/JUnit, Mockito and SL4J/1. JUnit_Basic Testing Exercises/Exercise 1/Exercise_1_Setting_Up_JUnit/src/main/java/Setting_Up_JUnit/Calculator.java
@@ -0,0 +1,11 @@
+package Setting_Up_JUnit;
+
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+}
